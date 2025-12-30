e36b826c993f6a4bcbc61c8d87316ebe994f8cef
nodestarQ <node@perfectqstudios.com>
2025-12-30 14:34:03 +0100

    fix: sigil is now renderin in the upper right corner with a margin of 1px

--- contracts/Sigil.sol
+++ contracts/Sigil.sol
@@ -1,19 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/utils/Base64.sol";
-
 contract Sigil {
     function svg(bytes32 h) public pure returns (string memory) {
+        uint256 xOff = 23;
+        uint256 yOff = 1;
+
         bytes memory out = abi.encodePacked(
-            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8" shape-rendering="crispEdges">'
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" shape-rendering="crispEdges">'
         );
 
         for (uint256 r; r < 8; ++r) {
             for (uint256 x; x < 4; ++x) {
                 uint256 i = r * 4 + x;
                 if ((uint8(h[i]) & 1) == 0) {
-                    out = abi.encodePacked(out, _px(x, r), _px(7 - x, r));
+                    out = abi.encodePacked(
+                        out,
+                        _px(xOff + x, yOff + r),
+                        _px(xOff + (7 - x), yOff + r)
+                    );
                 }
             }
         }
@@ -21,30 +26,24 @@ contract Sigil {
         return string(abi.encodePacked(out, "</svg>"));
     }
 
-    function testimg(bytes32 h) external pure returns (string memory) {
-        string memory s = svg(h);
-        string memory img = string(
-            abi.encodePacked(
-                "data:image/svg+xml;base64,",
-                Base64.encode(bytes(s))
-            )
-        );
-        return img;
-    }
-
     function _px(uint256 x, uint256 y) private pure returns (bytes memory) {
         return
             abi.encodePacked(
                 '<rect x="',
-                _d(x),
+                _u(x),
                 '" y="',
-                _d(y),
-                '" width="1" height="1" fill="#000000',
-                '" fill-opacity="0.7"/>'
+                _u(y),
+                '" width="1" height="1" fill="#000000" fill-opacity="0.7"/>'
             );
     }
 
-    function _d(uint256 v) private pure returns (string memory) {
-        return string(abi.encodePacked(bytes1(uint8(48 + v))));
+    function _u(uint256 v) private pure returns (string memory) {
+        if (v < 10) {
+            return string(abi.encodePacked(bytes1(uint8(48 + v))));
+        }
+        bytes memory b = new bytes(2);
+        b[0] = bytes1(uint8(48 + (v / 10)));
+        b[1] = bytes1(uint8(48 + (v % 10)));
+        return string(b);
     }
 }
